12e799b11d652c9993d0c3a5b7ff4bff41424dfc
henry22 <henry811010@gmail.com>
2017-02-27 00:37:41 +0800

    Add some style and show the description and link

--- src/components/SourceSelection.vue
+++ src/components/SourceSelection.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="sourceselection">
-    <h4>Select News source</h4>
-    <select class="form-control" v-on:change="sourceChanged">
-      <option v-bind:value="source.id" v-for="source in sources">{{source.name}}</option>
-    </select>
+    <div class="jumbotron">
+      <h2><span class="glyphicon glyphicon-list-alt"></span> News List</h2>
+      <h4>Select News source</h4>
+
+      <select class="form-control" v-on:change="sourceChanged">
+        <option v-bind:value="source.id" v-for="source in sources">{{source.name}}</option>
+      </select>
+
+      <div v-if="source">
+        <h6>{{source.description}}</h6>
+        <a v-bind:href="source.url" class="btn btn-primary" target="_blank">Go To {{source.name}} Website</a>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -17,9 +27,9 @@ export default {
     }
   },
   methods: {
-    sourceChanged: function (event) {
+    sourceChanged: function(event) {
       for (var i=0; i<this.sources.length; i++) {
-        if (this.sources[i] == event.target.value) {
+        if (this.sources[i].id == event.target.value) {
           this.source = this.sources[i]
         }
       }
@@ -29,7 +39,7 @@ export default {
     this.$http.get('https://newsapi.org/v1/sources?language=en')
               .then(response => {
                 this.sources = response.data.sources
-              });
+              })
   }
 }
 </script>
