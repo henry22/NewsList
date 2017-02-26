f01c6a0a8145576faeb43b56026b89c9b1516b05
henry22 <henry811010@gmail.com>
2017-02-27 00:51:32 +0800

    Get the articles from the api

--- src/components/Newslist.vue
+++ src/components/Newslist.vue
@@ -6,7 +6,21 @@
 
 <script>
   export default {
-    name: 'newslist'
+    name: 'newslist',
+    props: ['source'],
+    methods: {
+      updateSource: function (source) {
+        this.$http.get('https://newsapi.org/v1/articles?source=' + source + 'apiKey=ac11d5186d284152bd7271c2f707af78')
+                  .then(response => {
+                    this.articles = response.data.articles;
+                  });
+      }
+    },
+    watch: {
+      source: function (val) {
+        this.updateSource(val);
+      }
+    }
   }
 </script>
 
