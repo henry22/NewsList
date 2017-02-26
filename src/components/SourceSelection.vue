208a274dc39c2ff10dce8ff5bda0a3d0e6d63277
henry22 <henry811010@gmail.com>
2017-02-26 21:58:04 +0800

    Get the news api and show on the page

--- src/components/SourceSelection.vue
+++ src/components/SourceSelection.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="sourceselection">
-    SourceSelection
+    <h4>Select News source</h4>
+    <select class="form-control" v-on:change="sourceChanged">
+      <option v-bind:value="source.id" v-for="source in sources">{{source.name}}</option>
+    </select>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'sourceselection'
+  name: 'sourceselection',
+  data () {
+    return {
+      sources: [],
+      source: ''
+    }
+  },
+  methods: {
+    sourceChanged: function (event) {
+      for (var i=0; i<this.sources.length; i++) {
+        if (this.sources[i] == event.target.value) {
+          this.source = this.sources[i]
+        }
+      }
+    }
+  },
+  created: function() {
+    this.$http.get('https://newsapi.org/v1/sources?language=en')
+              .then(response => {
+                this.sources = response.data.sources
+              });
+  }
 }
 </script>
 
